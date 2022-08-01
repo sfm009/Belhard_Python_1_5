bd96f33b8f7cb946c90b3a94c16da932cab0fb40
unknown <sfm009@gmail.com>
2022-08-01 11:28:41 +0300

    equation

--- tasks/easy/if_elif_else/equation.py
+++ tasks/easy/if_elif_else/equation.py
@@ -37,6 +37,7 @@ var2 = (-b - кв.корень(d))/2a
 - resolve_equation(-1, -2, 15) -> (64, 2, -5, 3)
 - resolve_equation(1, 12, 36) -> (0, 1, -6, None)
 """
+from math import sqrt
 
 
 def resolve_equation(a: float, b: float, c: float) -> tuple:
@@ -51,10 +52,23 @@ def resolve_equation(a: float, b: float, c: float) -> tuple:
      первый корень, второй корень)
     :rtype: tuple
     """
-    d = None
-    n_var = None
-    var1 = None
-    var2 = None
+    #d = None
+    #n_var = None
+    #var1 = None
+    #var2 = None
+    d = (b ** 2) - (4 * a * c)
+    if d > 0:
+        n_var = 2
+        var1 = (-b + sqrt(d)) / (2 * a)
+        var2 = (-b - sqrt(d)) / (2 * a)
+    elif d == 0:
+        n_var = 1
+        var1 = (-b + sqrt(d)) / (2 * a)
+        var2 = None
+    elif d < 0:
+        n_var = None
+        var1 = None
+        var2 = None
     return d, n_var, var1, var2
 
 
