4a83ab8142190d6ff75f0a4077394de477260351
unknown <sfm009@gmail.com>
2022-08-05 15:22:06 +0300

    selection_sort

--- tasks/hard/selection_sort.py
+++ tasks/hard/selection_sort.py
@@ -19,6 +19,12 @@
 
 
 def selection_sort(array: list) -> list:
+    for i in range(len(array) - 1):
+        min_index = i
+        for j in range(i + 1, len(array) - 1):
+            if array[j] < array[min_index]:
+                min_index = j
+        array[i], array[min_index] = array[min_index], array[i]
     return array
 
 
